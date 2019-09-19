7dbc3c44bfdae5d9ccad0c53d9345834b3d9f747
Doan Van Hieu <doanhieu.bl.hn@gmail.com>
2019-09-19 11:59:31 +0700

    max min

--- search_max_min.js
+++ search_max_min.js
@@ -1,13 +1,12 @@
 function Search_min(arr) {
-    let min =arr[0];
-    for (let i =0 ; i<arr.length; i++){
-        if(arr[i]<arr[0]){
-            arr[i] = min;
+    let min = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min=arr[i];
         }
     }
     return min;
 };
-
 
 function Search_max(arr) {
     var max = arr[0];
@@ -21,27 +20,36 @@ function Search_max(arr) {
 
 
 //tao ra  mảng ngẫu  nhiện
-let arry_Radom =[];
-    for(let i = 0; i <100;i++){
-        arry_Radom[i] = Math.floor(Math.random()* 101);
-    };
 
 
 function dispArr(arr) {
-    for (var i = 0; i < arr.length; ++i) {
-       let putstr = arr[i] + " ";
-        if(i%10==9){
-            document.write(putstr+"\n");
+    let cot=10;
+    document.getElementById('result').innerHTML='';
+    for (var i = 0; i < arr.length; i++) {
+        document.getElementById('result').innerHTML+= arr[i] + " ";
+        if (i % cot === 8) {
+            document.getElementById('result').innerHTML+=arr[++i]+'<br>';
         }
     }
-    if(i%10!=0){
-        document.write(putstr+"\n");
-    }
+    // if (i % 10 !== 0) {
+    //     document.getElementById('result').innerHTML+="<br>";
+    // }
 }
-dispArr(arry_Radom);
-let minValue = Search_min(arry_Radom);
-let maxValue = Search_max(arry_Radom);
-document.write("<br/>");
-document.write("The minimum valuse is "+minValue);
-document.write("The maximum valuse is "+maxValue);
+function randomArray() {
+    let arr = [];
+    for (let i = 0; i < 100; i++) {
+        arr[i] = Math.floor(Math.random() * 90+10);
+    }
+    return arr;
+}
+function main() {
+    let arry_Radom = randomArray();
+    dispArr(arry_Radom);
+    let minValue = Search_min(arry_Radom);
+    let maxValue = Search_max(arry_Radom);
+    document.getElementById('result').innerHTML+="<br/>";
+    document.getElementById('result').innerHTML+="The minimum valuse is " + minValue;
+    document.getElementById('result').innerHTML+="<br/>";
+    document.getElementById('result').innerHTML+="The maximum valuse is " + maxValue+'<br>';
 
+}
